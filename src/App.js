34e18750469f1fd008612dac564ab6b8c3e7da01
Vani Swamy <vani.swamy@capgemini.com>
2020-04-21 10:04:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Vani All is best   </p>
+       ISP React Demo   </p>
         <a
           className="App-link"
           href="https://reactjs.org"
