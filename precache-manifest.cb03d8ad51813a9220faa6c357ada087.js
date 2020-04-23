712cf0b28d5f7b9ec1516a604937594f6b8e66de
Vani Swamy <vani.swamy@capgemini.com>
2020-04-23 08:57:04 +0200

    copied build to master

---
+++ precache-manifest.cb03d8ad51813a9220faa6c357ada087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf99793834945e9816fe216c481e66c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ad8d608e4372ec2becf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "72f2f62a97bd3363d83e",
+    "url": "/static/js/2.fd2b3e82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd2b3e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad8d608e4372ec2becf",
+    "url": "/static/js/main.bbdc7d8d.chunk.js"
+  },
+  {
+    "revision": "a4ab1066b6c825064e83",
+    "url": "/static/js/runtime-main.13652d16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
